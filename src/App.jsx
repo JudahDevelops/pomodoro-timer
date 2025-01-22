0e8a8416e0b4c53f13b7a9a086e753e0b14a3192
JudahDevelops <judahdevelops@gmail.com>
2025-01-22 08:27:35 +0100

    ran outta time, i managed to get a timer working and im trying to progress from there

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component, useState } from 'react'
 import './App.css'
+import React,{ component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-  return (
-    <>
+  render() {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Pomodoro />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    )
+  }
+}
+
+class Pomodoro extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      isOn: true
+    }
+    this.handleStartStop = this.handleStartStop.bind(this);
+  }
+
+  handleBreakChange = (event) => {
+    this.setState({
+      breakLength: event.target.value
+    })
+  }
+
+  handleSessionChange = (event) => {
+    this.setState({
+      sessionLength: event.target.value
+    })
+  }
+
+  handleStartStop = () => {
+    this.setState({
+      isOn: !this.state.isOn
+    })
+  }
+
+  render() {
+    let seconds = this.state.sessionLength * 60;
+
+    setInterval(updateTime, 1000);
+
+    function updateTime() {
+      let minutes = seconds / 60;
+      document.getElementById('countdown').innerHTML = `${Math.floor(minutes)}:${(seconds % 60) < 10 ? `0${seconds % 60}`:seconds % 60}`;
+      seconds--;
+    }
+
+
+    return (
+      <div>
+        <h1>Pomodoro Timer</h1>
+        <div id='lengths'>
+          <div id='break'>
+            <h2><label for="break">Break Length</label></h2>
+            <input type="number" id="break" name="break" min="1" max="100" step="1" value={this.state.breakLength} onChange={this.handleBreakChange}></input>
+          </div>
+          <div id='session'>
+            <h2><label for="session">Session Length</label></h2>
+            <input type="number" id="session" name="session" min="1" max="100" step="1" value={this.state.sessionLength} onChange={this.handleSessionChange}></input>
+          </div>
+        </div>
+        <div id='timer'>
+          <h2 id='timer-label'>Session</h2>
+          <h2 id='countdown'></h2>
+          <div id='controls'>
+            <button id='start_stop' onClick={this.handleStartStop}>Start/Stop</button>
+            <button id='reset'>Reset</button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
 
 export default App
