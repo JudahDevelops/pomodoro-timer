56b0b5003da66988fd73d2fed6d42325dce7c96c
JudahDevelops <judahdevelops@gmail.com>
2025-01-24 08:22:29 +0100

    restarted and made lots of progress, should be done by the next session

--- src/App.jsx
+++ src/App.jsx
@@ -20,86 +20,96 @@ class Pomodoro extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      breakLength: 5,
       sessionLength: 25,
-      isOn: true
+      breakLength: 5,
+      isOn: false,
+      type: 'Session'
     }
-    this.handleStartStop = this.handleStartStop.bind(this);
   }
 
-  handleBreakChange = (event) => {
-    this.setState({
-      breakLength: event.target.value
-    })
-  }
-
-  handleSessionChange = (event) => {
-    this.setState({
-      sessionLength: event.target.value
-    })
+  handleLengthChange = (e) => {
+    const id = e.target.id
+    const sessionLength = this.state.sessionLength
+    const breakLength = this.state.breakLength
+    if (id === 'break-decrement' && breakLength > 1) {
+      this.setState({ breakLength: breakLength - 1 })
+    } else if (id === 'break-increment' && breakLength < 60) {
+      this.setState({ breakLength: breakLength + 1 })
+    } else if (id === 'session-decrement' && sessionLength > 1) {
+      this.setState({ sessionLength: sessionLength - 1 })
+    } else if (id === 'session-increment' && sessionLength < 60) {
+      this.setState({ sessionLength: sessionLength + 1 })
+    }
   }
 
   handleStartStop = () => {
-    this.setState({
-      isOn: !this.state.isOn
-    });
-    document.getElementById('start_stop').innerText = this.state.isOn ? 'Stop' : 'Start';
+    this.setState({ isOn: !this.state.isOn })
   }
 
+render() {
+  //TODO
+  //Implement the start stop functionality that doesn't just restart the timer
+  //implement the reset functionality that changes type to session and resets the timer
+  //implement the audio alert when the timer reaches 0
+  const mins = document.getElementById('minutes');
+  const secs = document.getElementById('seconds');
 
-  render() {
-    let seconds = this.state.sessionLength * 60;
-    
+  let seconds;
+  if (this.state.type === 'Session') {
+    seconds = this.state.sessionLength * 60;
+  } else if (this.state.type === 'Break') {
+    seconds = this.state.breakLength * 60;
+  }
 
-   const updateTime = () => {
+  const updateTimer = () => {
+    if (this.state.isOn) {
+      if (seconds < 0) {
+        if (this.state.type === 'Session') {
+          this.setState({ type: 'Break' });
+          seconds = this.state.breakLength * 60;
+        } else if (this.state.type === 'Break') {
+          this.setState({ type: 'Session' });
+          seconds = this.state.sessionLength * 60;
+        }
+      }
+      seconds --;
+      let currentSecs = seconds % 60;
       let minutes = Math.floor(seconds / 60);
-      document.getElementById('countdown').innerHTML = `${minutes}:${seconds % 60 < 10 ? '0' + seconds % 60 : seconds % 60}`;
-      seconds--;
-   } 
+      mins.innerText = `${minutes}`;
+      secs.innerText = `${currentSecs < 10 ? '0' + currentSecs : currentSecs}`;
+    }
+  }
 
-   isOn ? clearInterval(updateTime) : setInterval(updateTime, 1000);
+  setInterval(updateTimer, 1000);
 
-
-
-
-  
-
-
-
-
-
-
-    //if ison is true
-      //setInterval(updateTime, 1000)
-    //else
-      //clearInterval(updateTime)
-    //if reset is pressed
-      //set seconds to sessionLength * 60
-      //set session length to 25
-      //set break length to 5
-
-
-
-    return (
+  return (
       <div>
         <h1>Pomodoro Timer</h1>
         <div id='lengths'>
-          <div id='break'>
-            <h2><label for="break">Break Length</label></h2>
-            <input type="number" id="break" name="break" min="1" max="100" step="1" value={this.state.breakLength} onChange={this.handleBreakChange}></input>
+          <div class='length-cont'>
+            <h3>Break Length</h3>
+            <div class='change-cont'>
+              <button id='break-decrement' className='changeBtn' onClick={this.handleLengthChange}>-</button>
+              <span id='break-length'>{this.state.breakLength}</span>
+              <button id='break-increment' className='changeBtn' onClick={this.handleLengthChange}>+</button>
+            </div>
           </div>
-          <div id='session'>
-            <h2><label for="session">Session Length</label></h2>
-            <input type="number" id="session" name="session" min="1" max="100" step="1" value={this.state.sessionLength} onChange={this.handleSessionChange}></input>
+          <div class='length-cont'>
+            <h3>Session Length</h3>
+            <div class='change-cont'>
+              <button id='session-decrement' className='changeBtn' onClick={this.handleLengthChange}>-</button>
+              <span id='session-length'>{this.state.sessionLength}</span>
+              <button id='session-increment' className='changeBtn' onClick={this.handleLengthChange}>+</button>
+            </div>
           </div>
         </div>
         <div id='timer'>
-          <h2 id='timer-label'>Session</h2>
-          <h2 id='countdown'></h2>
-          <div id='controls'>
-            <button id='start_stop' onClick={this.handleStartStop}>Stop</button>
-            <button id='reset'>Reset</button>
-          </div>
+          <h2>{this.state.type}</h2>
+          <span id='time-left'><span id='minutes'>{this.state.sessionLength}</span>:<span id='seconds'>00</span></span>
+        </div>
+        <div id='controls'>
+          <button id='start_stop' class='ctrlBtn' onClick={this.handleStartStop}>Start/Stop</button>
+          <button id='reset' class='ctrlBtn'>Reset</button>
         </div>
       </div>
     )
